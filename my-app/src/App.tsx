fd7cbb90a5d136afc5e503743eb8cf8b749e0937
Gift David <giftdavid@Gifts-Air.lan>
2022-12-14 18:51:08 +0100

    feat:login page created

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Components/organisms/nav";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Routes>
+        <Route element={<Home />} />
+        <Route element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- my-app/src/Components/organisms/nav.tsx
+++ my-app/src/Components/organisms/nav.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid grey;
+  padding: 10px;
+  nav {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
 
 const Nav = () => {
-    return (
-        <header>
-            <nav>
-                <Link to={"/"} >Gift David</Link>
-                <Link to={"/login"}>Login</Link>
-            </nav>
-        </header>
-    );
+  return (
+    <HeaderStyle>
+      <nav>
+        <Link to={"/"}>Gift David</Link>
+        <Link to={"/login"}>Login</Link>
+      </nav>
+    </HeaderStyle>
+  );
 };
 
 export default Nav;
