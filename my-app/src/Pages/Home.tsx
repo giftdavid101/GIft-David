e50e5161a96a1805ee63857f3e081e64b4de4862
Gift David <giftdavid@Gifts-Air.lan>
2022-12-14 18:53:56 +0100

    feat:Home

---
+++ my-app/src/Pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomeStyle = styled.div``;
+const Home = () => {
+  return <HomeStyle>Home</HomeStyle>;
+};
+
+export default Home;
